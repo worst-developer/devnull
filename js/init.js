d21d2c32f03ad3a216d952fc933c89ddbd0f3d67
Yarossia <gtibmx@yandex.ru>
2015-07-28 15:44:03 +0300

    animation does not working

--- js/init.js
+++ js/init.js
@@ -1,8 +1,28 @@
-$(document).ready(function() {
-	$('#demo').addClass('animated hinge');
-});
-var str = ["Hello world!!!!!!!!!", "lol", "tizh programmer", "fuck yeah!"];
 
-function myFunction() {
-    document.getElementById("demo").innerHTML = str[Math.floor(Math.random()*str.length)];
-};
+var str = ["Hello world!!!!!!!!!", "lol", "tizh programmer", "fuck yeah!"];
+//
+//function myFunction() {
+//    document.getElementById("demo").innerHTML = str[Math.floor(Math.random()*str.length)];
+////    $(document).ready(function() {
+//        $('#demo').addClass('animated hinge');
+//        $(this).removeClass('animated hinge');
+//    })};
+////$(document).ready(function() {
+////    $('.button').on("click", function() {
+////        $(this).addClass('animated hinge').one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd", function(){
+////            $(this).removeClass();
+////        });
+////    });
+////});
+//////$(document).ready(function() {
+////
+//////});
+
+$(document).ready(function() {
+    $('.button').on("click", function () {
+        $('#text').text(str[Math.floor(Math.random()*str.length)]);
+        $('#text').addClass('animated bounce').one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd", function(){
+            $(this).removeClass();
+        });
+        });
+ });
