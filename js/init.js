eefb7e66a9778fcbaff49eab04a9d0e8c5e10494
Yarossia <gtibmx@yandex.ru>
2015-07-28 17:25:39 +0300

    beta 0.9

--- js/init.js
+++ js/init.js
@@ -1,30 +1,13 @@
 
-var str = ["Hello world!!!!!!!!!", "lol", "tizh programmer", "fuck yeah!"];
-//
-//function myFunction() {
-//    document.getElementById("demo").innerHTML = str[Math.floor(Math.random()*str.length)];
-////    $(document).ready(function() {
-//        $('#demo').addClass('animated hinge');
-//        $(this).removeClass('animated hinge');
-//    })};
-////$(document).ready(function() {
-////    $('.button').on("click", function() {
-////        $(this).addClass('animated hinge').one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd", function(){
-////            $(this).removeClass();
-////        });
-////    });
-////});
-//////$(document).ready(function() {
-////
-//////});
-
+var str = ["HELLO WORLD, HELLO, HELLO, MOTHERFUCKING WORLD!!!", "BUT YOU ARE DEVELOPER", "CEEP CALM AND DEBUG YOUR CODE", "STUPID JAVA!!!", "f=ma", "REQUEST - RESPONSE", "DEADLINE", "TESTERS GONNA TEST", "I'M NOT RESPONSIBLE FOR THIS CODE. THEY FORCED ME TO WRITE IT AGAINST MY WILL", "MAGIC DO NOT TOUCH!!!", "If you're reading this, then you moved to my last project. I'm sorry. Good luck!"];
+var colours = ["#a67d63", "#bcada3", "#d8ccf6", "#d8efb4", "#ff4c2d"];
 $(document).ready(function() {
     $('.button').on("click", function () {
         $('#text').text(str[Math.floor(Math.random()*str.length)]);
-        $('#text').addClass('animated bounce');
+        $('body').css("background-color", colours[Math.floor(Math.random()*colours.length)])
+        $('#text').addClass('animated flipInX');
         setTimeout(function(){
             $('#text').removeClass();
-        }, 2000);
-
+        }, 1050);
         });
  });
