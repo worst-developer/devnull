1da6ec9c96448fe087fbd0ab49a62520f2056bf0
Yarossia <gtibmx@yandex.ru>
2015-10-31 13:21:49 +0200

    lol

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
 
-var str = ["HELLO WORLD, HELLO, HELLO, MOTHERFUCKING WORLD!!!", "BUT YOU ARE DEVELOPER", "CEEP CALM AND DEBUG YOUR CODE", "STUPID JAVA!!!", "f=ma", "REQUEST - RESPONSE", "DEADLINE", "TESTERS GONNA TEST", "I'M NOT RESPONSIBLE FOR THIS CODE. THEY FORCED ME TO WRITE IT AGAINST MY WILL", "MAGIC DO NOT TOUCH!!!", "If you're reading this, then you moved to my last project. I'm sorry. Good luck!"];
+var str = ["Сейчас точно вверх пойдет!", "Поставлю в 2 раза больше и точно отиграюсь","Два месяца и на квартиру заработаю, как два пальца об асфальт", 'Опять депозит слил', 'Учить для слабых, я хочу деньги сдесь и сейчас'];
 var colours = ["#a67d63", "#bcada3", "#d8ccf6", "#d8efb4", "#ff4c2d"];
 $(document).ready(function() {
     $('.button').on("click", function () {
